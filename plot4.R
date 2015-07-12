ef6d64841e88acfd8dab919ab0c416944c46f480
Sergey Charkin <scharkin@deloitte.com>
2015-07-12 19:10:14 -0400

    plot4.R creates plot4.png, no legend border

--- plot4.R
+++ plot4.R
@@ -1,9 +1,12 @@
 # read rough d subset having needed data
 d <- read.csv("household_power_consumption.txt", sep=";", quote="", na.strings="?", nrows = 70000)
+
 # filter out d2 data subset
 d2 <- d[d$Date == "1/2/2007" | d$Date == "2/2/2007",]
+
 # make x from date and time
 datetime <- as.POSIXct(paste(d2$Date, d2$Time), format="%d/%m/%Y %H:%M:%S")
+
 # open png graph device
 png(filename = "plot4.png", width = 480, height = 480, units = "px")
 
@@ -18,6 +21,7 @@ plot(datetime, d2$Voltage, type = "l", ylab = "Voltage")
 plot(datetime, d2$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(datetime, d2$Sub_metering_2, type = "l", col = "red")
 lines(datetime, d2$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = "solid", col = c("black","red","blue"), bty="n", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 plot(datetime, d2$Global_reactive_power, type = "l", ylab = "Global_reactive_power")
 
