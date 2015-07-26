8f2a7659aa14a5efe2f7272a048d32cd893c0a54
Sergey Charkin <sergey.charkin@gmail.com>
2015-07-26 12:19:44 -0400

    project 2 graph 1

---
+++ CourseProject2/plot1.R
@@ -0,0 +1,16 @@
+# read data
+d <- readRDS("summarySCC_PM25.rds")
+
+# aggregate emissions by year
+r <- aggregate(d$Emissions, list(year = d$year), sum)
+
+# open png graph device
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+# create graph
+barplot(r$x, names = r$year, xlab = "Years",
+	ylab = "Total PM2.5 emission from all sources, tons",
+	main = "Total emissions from PM2.5 in the United States\ndecreased from 1999 to 2008")
+
+# finish
+dev.off()
