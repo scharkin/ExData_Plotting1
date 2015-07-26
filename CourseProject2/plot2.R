5d69910df675582309c03191b3ff8f4b7d0a8606
Sergey Charkin <sergey.charkin@gmail.com>
2015-07-26 12:47:03 -0400

    plot2 for project2

---
+++ CourseProject2/plot2.R
@@ -0,0 +1,19 @@
+# read data
+d <- readRDS("summarySCC_PM25.rds")
+
+# filter out d2 data subset for Baltimore City, Maryland
+d2 <- d[d$fips == 24510,]
+
+# aggregate emissions for Baltimore City by year
+r <- aggregate(d2$Emissions, list(year = d2$year), sum)
+
+# open png graph device
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+
+# create graph
+barplot(r$x, names = r$year, xlab = "Years",
+	ylab = "Total PM2.5 emission the Baltimore City, tons",
+	main = "Total emissions from PM2.5 in the Baltimore City, Maryland")
+
+# finish
+dev.off()
